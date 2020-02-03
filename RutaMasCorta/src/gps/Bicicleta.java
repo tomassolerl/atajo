1e4155be126963cd905592639da9a731fbe80a10
Tomas <tomas@tomas.com>
2020-02-03 10:20:01 +0100

    Modificaciones realizadas

--- RutaMasCorta/src/gps/Bicicleta.java
+++ RutaMasCorta/src/gps/Bicicleta.java
@@ -19,12 +19,9 @@ public class Bicicleta {
         int sum2 = 0;
         int sum3 = 0;
         
-        for(int i=0; i<camino1.length; i++)
-        {
-            sum1 += camino1[i];
-            sum2 += camino2[i];
-            sum3 += camino3[i];
-        }
+        sum1 = sumaArray(camino1);
+        sum2 = sumaArray(camino2);
+        sum3 = sumaArray(camino3);
         
         if(sum1 < sum2 && sum1 < sum3)
         {
@@ -51,9 +48,18 @@ public class Bicicleta {
         
     }
 
+	public static int sumaArray(int[] camino) {
+		int suma=0;
+		for(int i=0; i<camino.length; i++)
+        {
+           suma += camino[i];
+        }
+		return suma;
+	}
+
     public static void main(String[] args) {
         Bicicleta bici = new Bicicleta();
-        int[] camino1 = {12,34,21,46,25};
+        int[] camino1 = {12,34,21,46,25,200,100};
         int[] camino2 = {24,1,5,64,10,15,21};
         int[] camino3 = {1,5,6,7,8,10,20,46,104};
         System.out.println("El camino más corto es:"+Bicicleta.rutaCorta(camino1,camino2,camino3)+".");
